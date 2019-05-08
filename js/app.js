b09d9fd47ecdd6df1d43bb48cafede4a08246c26
Martin Balke <Mrtnbalke@gmail.com>
2019-05-07 18:39:54 -0700

    Added javascript functionality to the scoreboard

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ function guessingGame(guess){
     userScore++;
     alert('That\'s right! now you\'re ' + userScore + ' out of 5!');
     console.log('User guessed ' + guess + ' they were right and currently have a score of ' + userScore + ' out of 5');
+    document.getElementById('scoreBoardCounter').innerHTML = userScore + ' out of 5';
   }
   else if (guess.toLocaleLowerCase() === 'n' || guess.toLocaleLowerCase() === 'no' ){
     alert('Sorry you guessed wrong! Right now you\'re ' + userScore + ' out of 5');
@@ -18,19 +19,24 @@ function guessingGame(guess){
   }
   
 }
-var myNameGuess = prompt('Lets play a guessing game so that we can learn about me the author! The first question is, is my name Martin?');
-guessingGame(myNameGuess);
+setTimeout(function(){
+  var myNameGuess = prompt('Lets play a guessing game so that we can learn about me the author! The first question is, is my name Martin?');
+  guessingGame(myNameGuess);
 
-var myJobGuess = prompt('Okay, on to the next question. Is my job working in food?');
-guessingGame(myJobGuess);
+  var myJobGuess = prompt('Okay, on to the next question. Is my job working in food?');
+  guessingGame(myJobGuess);
 
-var myFoodGuess = prompt('You\'ve been working hard so I will throw you an easy question on this next one! Do I like to eat pizza?');
-guessingGame(myFoodGuess);
+  var myFoodGuess = prompt('You\'ve been working hard so I will throw you an easy question on this next one! Do I like to eat pizza?');
+  guessingGame(myFoodGuess);
 
-var myMusicGuess  = prompt('Okay how about a harder question. Is Disco my favorite kind of music?');
-guessingGame(myMusicGuess);
+  var myMusicGuess  = prompt('Okay how about a harder question. Is Disco my favorite kind of music?');
+  guessingGame(myMusicGuess);
 
-var myCodingGuess = prompt('You\'re almost at the end! Last question this time. Do you think I love coding!?');
-guessingGame(myCodingGuess);
+  var myCodingGuess = prompt('You\'re almost at the end! Last question this time. Do you think I love coding!?');
+  guessingGame(myCodingGuess);
+
+
+
+}, 200);
 
 
