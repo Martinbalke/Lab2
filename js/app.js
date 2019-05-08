82e236640491c367ceedc19043b9949daf4af231
Martin Balke <Mrtnbalke@gmail.com>
2019-05-07 17:46:36 -0700

    Laying down the foundation for color scheme, background, and font styling

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@
 // initialize the user's guessing score
 var userScore = 0;
 
-//prompt the user to answer the first question
 
-
+// function takes the user input from a prompt and returns a correct or incorrect alert then updates the users score to reflect their guess
 function guessingGame(guess){
   if (guess.toLocaleLowerCase() === 'y' || guess.toLocaleLowerCase() === 'yes' ){
     userScore++;
@@ -15,7 +14,7 @@ function guessingGame(guess){
     alert('Sorry you guessed wrong! Right now you\'re ' + userScore + ' out of 5');
     console.log('User guessed ' + guess + ' they were wrong and currently have a score of ' + userScore + ' out of 5');
   } else{
-    alert('Sorry, please guess yes or no');
+    alert('Sorry, please guess yes or no! You missed your point for this round.');
   }
   
 }
@@ -24,38 +23,14 @@ guessingGame(myNameGuess);
 
 var myJobGuess = prompt('Okay, on to the next question. Is my job working in food?');
 guessingGame(myJobGuess);
-//check for answer and then update userScore based on right and wrong
-// if (myNameGuess.toLocaleLowerCase() === 'yes' || myNameGuess.toLocaleLowerCase() === 'y'){
-//   userScore++;
-//   alert('That\'s right! now you\'re ' + userScore + ' out of 5!');
-//   console.log('User guessed ' + myNameGuess + ' they were right and currently have a score of ' + userScore + ' out of 5');
-// }
-// else if (myNameGuess.toLocaleLowerCase() === 'no' || myNameGuess.toLocaleLowerCase() === 'n'){
-//   alert('Sorry you guessed wrong! Right now you\'re ' + userScore + ' out of 5');
-//   console.log('User guessed ' + myNameGuess + ' they were wrong and currently have a score of ' + userScore + ' out of 5');
-// }
-
-
-
-// if (myJobGuess.toLocaleLowerCase() === 'yes' || myJobGuess.toLocaleLowerCase() === 'y'){
-//   userScore++;
-//   alert('That\'s right! now you\'re ' + userScore + ' out of 5!');
-//   console.log('User guessed ' + myJobGuess + ' they were right and currently have a score of ' + userScore + ' out of 5');
-// }
-// else if (myJobGuess.toLocaleLowerCase() === 'no' || myJobGuess.toLocaleLowerCase() === 'n'){
-//   alert('Sorry you guessed wrong! Right now you\'re ' + userScore + ' out of 5');
-//   console.log('User guessed ' + myJobGuess + ' they were wrong and currently have a score of ' + userScore + ' out of 5');
-// }
 
 var myFoodGuess = prompt('You\'ve been working hard so I will throw you an easy question on this next one! Do I like to eat pizza?');
+guessingGame(myFoodGuess);
 
-// if (myFoodGuess.toLocaleLowerCase() === 'yes' || myFoodGuess.toLocaleLowerCase() === 'y'){
-//   userScore++;
-//   alert('That\'s right! now you\'re ' + userScore + ' out of 5!');
-//   console.log('User guessed ' + myFoodGuess + ' they were right and currently have a score of ' + userScore + ' out of 5');
-// }
-// else if (myFoodGuess.toLocaleLowerCase() === 'no' || myFoodGuess.toLocaleLowerCase() === 'n'){
-//   alert('Sorry you guessed wrong! Right now you\'re ' + userScore + ' out of 5');
-//   console.log('User guessed ' + myFoodGuess + ' they were wrong and currently have a score of ' + userScore + ' out of 5');
-// }
+var myMusicGuess  = prompt('Okay how about a harder question. Is Disco my favorite kind of music?');
+guessingGame(myMusicGuess);
+
+var myCodingGuess = prompt('You\'re almost at the end! Last question this time. Do you think I love coding!?');
+guessingGame(myCodingGuess);
+
 
