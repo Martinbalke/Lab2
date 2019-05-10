e2b7ffcb4781c1fb28334a047bd8689e183b8023
Martin Balke <Mrtnbalke@gmail.com>
2019-05-09 20:25:11 -0700

    added a for loop for country guessing

--- js/app.js
+++ js/app.js
@@ -20,25 +20,35 @@ function guessingGame(guess){
   }
   
 }
-// Timeout the prompt so html runs first, then ask a series of prompts and run them through the guessing game function.
-setTimeout(function(){
-  var myNameGuess = prompt('Lets play a guessing game so that we can learn about me the author! The first question is, is my name Martin?');
-  guessingGame(myNameGuess);
 
-  var myJobGuess = prompt('Okay, on to the next question. Is my job working in food?');
-  guessingGame(myJobGuess);
+//This code block asks a serious of prompting questions to the user and then runs their answer through the guessingGame function
+var myNameGuess = prompt('Lets play a guessing game so that we can learn about me the author! The first question is, is my name Martin?');
+guessingGame(myNameGuess);
 
-  var myFoodGuess = prompt('You\'ve been working hard so I will throw you an easy question on this next one! Do I like to eat pizza?');
-  guessingGame(myFoodGuess);
+var myJobGuess = prompt('Okay, on to the next question. Is my job working in food?');
+guessingGame(myJobGuess);
 
-  var myMusicGuess  = prompt('Okay how about a harder question. Is Disco my favorite kind of music?');
-  guessingGame(myMusicGuess);
+var myFoodGuess = prompt('You\'ve been working hard so I will throw you an easy question on this next one! Do I like to eat pizza?');
+guessingGame(myFoodGuess);
 
-  var myCodingGuess = prompt('You\'re almost at the end! Last question this time. Do you think I love coding!?');
-  guessingGame(myCodingGuess);
+var myMusicGuess  = prompt('Okay how about a harder question. Is Disco my favorite kind of music?');
+guessingGame(myMusicGuess);
 
+var myCodingGuess = prompt('You\'re almost at the end! Last question this time. Do you think I love coding!?');
+guessingGame(myCodingGuess);
 
+//for loop gives the user six chances to guess a country in which i've lived in
+for(var i = 0; i < 7; i++){
+  var myCountryGuess = prompt('Can you guess one of the countries I\'ve lived in besides the United states? You get six tries!');
+  if(myCountryGuess.toLocaleLowerCase() === 'germany' || myCountryGuess.toLocaleLowerCase() === 'canada'){
+    alert('You got it right!');
+    break;
+  } else if(i === 6){
+    alert('Sorry you used up all of your guesses');
+  }else {
+    alert('Nice try but I haven\'t lived there! Guess again');
+  }
+}
 
-}, 200);
 
 
